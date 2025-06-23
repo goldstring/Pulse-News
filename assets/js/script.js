2107e135c4f03ad3b7307a0f4e5fa4944af53968
goldstring <yashp7962@gmail.com>
2025-06-23 22:30:12 +0530

    hbibaisg

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,21 @@ $(document).ready(function () {
         }
 
     });
+
+    $("#newsCardSlider").owlCarousel({
+        loop: true,
+        margin: 16,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: { items: 1 },
+            576: { items: 1 },
+            768: { items: 2 },
+            992: { items: 4 }
+        }
+    });
+
+
 });
